4f666d8b645fadcd83a3fd26faccd27409feda1f
Corbin Broadhead <poujmail@gmail.com>
2023-02-04 16:34:49 -0700

    orders went smoothly so that's nice.

--- orders.js
+++ orders.js
@@ -30,8 +30,19 @@
 */
 
 //CODE HERE
+class Ticket {
+    constructor(items, orderTime, customerId) {
+      this.items = items;
+      this.orderTime = orderTime;
+      this.customerId = customerId;
+      this.status = 'queued';
+    }
 
-
+    updateStatus(newStatus) {
+        this.status = newStatus;
+        console.log(`The order for customer ${this.customerId} is now ${this.status}.`);
+        }
+    }
 
 /*
     Create a new instance of your class.
@@ -45,7 +56,7 @@
 */
 
 //CODE HERE
-
+let firstTicket = new Ticket('pizza, breadsticks', '4:32 PM', 820);
 
 /*
     Call the `updateStatus` method on
@@ -54,3 +65,4 @@
 */
 
 //CODE HERE
+firstTicket.updateStatus('cooking');
